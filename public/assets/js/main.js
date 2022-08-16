1374f3e82ad28f3baf688def3f7ec8a48a8f7cff
reoccurcat <reoccurcat@gmail.com>
2022-08-16 18:50:20 -0500

    misc

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -5,8 +5,8 @@ URLArray = [];
 URLArray[0] = 'https://madokamagicausa.com';
 URLArray[1] = 'https://genrandom.com/cats';
 
-const form = document.querySelector('.form2');
-const input = document.querySelector('.input2');
+const form = document.getElementById('form2');
+const input = document.getElementById('input2');
 
 const TxtType = function (el, toRotate, period) {
     this.toRotate = toRotate;
@@ -144,21 +144,19 @@ function smth() {
     x1.removeAttribute('hidden')
 }
 
-function loaded() {
-    form.addEventListener('submit', async event => {
-        event.preventDefault();
-        window.navigator.serviceWorker.register('./sw.js', {
-            scope: __uv$config.prefix
-        }).then(() => {
-            let url = input.value.trim();
-            if (!isUrl(url)) url = 'https://www.google.com/search?q=' + url;
-            else if (!(url.startsWith('https://') || url.startsWith('http://'))) url = 'http://' + url;
+form.addEventListener('submit', async event => {
+    event.preventDefault();
+    window.navigator.serviceWorker.register('./sw.js', {
+        scope: __uv$config.prefix
+    }).then(() => {
+        let url = input.value.trim();
+        if (!isUrl(url)) url = 'https://www.google.com/search?q=' + url;
+        else if (!(url.startsWith('https://') || url.startsWith('http://'))) url = 'http://' + url;
 
 
-            window.location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
-        });
+        window.location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
     });
-}
+});
 
 function isUrl(val = ''){
     return /^http(s?):\/\//.test(val) || val.includes('.') && val.substr(0, 1) !== ' ';
