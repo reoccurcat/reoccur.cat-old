2ec45f7e3899b9d854b5c302289a9f29749f351f
reoccurcat <reoccurcat@gmail.com>
2022-08-16 18:45:17 -0500

    misc

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -144,19 +144,21 @@ function smth() {
     x1.removeAttribute('hidden')
 }
 
-form.addEventListener('submit', async event => {
-    event.preventDefault();
-    window.navigator.serviceWorker.register('./sw.js', {
-        scope: __uv$config.prefix
-    }).then(() => {
-        let url = input.value.trim();
-        if (!isUrl(url)) url = 'https://www.google.com/search?q=' + url;
-        else if (!(url.startsWith('https://') || url.startsWith('http://'))) url = 'http://' + url;
+function loaded() {
+    form.addEventListener('submit', async event => {
+        event.preventDefault();
+        window.navigator.serviceWorker.register('./sw.js', {
+            scope: __uv$config.prefix
+        }).then(() => {
+            let url = input.value.trim();
+            if (!isUrl(url)) url = 'https://www.google.com/search?q=' + url;
+            else if (!(url.startsWith('https://') || url.startsWith('http://'))) url = 'http://' + url;
 
 
-        window.location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
+            window.location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
+        });
     });
-});
+}
 
 function isUrl(val = ''){
     return /^http(s?):\/\//.test(val) || val.includes('.') && val.substr(0, 1) !== ' ';
