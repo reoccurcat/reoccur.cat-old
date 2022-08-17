019df4678089f29d4545abf70b7e19ceefa6c9b0
reoccurcat <reoccurcat@gmail.com>
2022-08-16 20:08:35 -0500

    misc

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -135,10 +135,34 @@ function myFunction2() {
 }
 
 function smth() {
+
     const x2 = document.getElementById("maindiv");
     const x1 = document.getElementById("uv")
     x2.setAttribute('hidden', '')
     x1.removeAttribute('hidden')
+    const form = document.getElementById('form2');
+    console.log(form)
 }
 
+const form = document.getElementById('form2');
+const input = document.getElementById('input2');
 
+console.log(form)
+
+form.addEventListener('submit', async event => {
+    event.preventDefault();
+    window.navigator.serviceWorker.register('./sw.js', {
+        scope: __uv$config.prefix
+    }).then(() => {
+        let url = input.value.trim();
+        if (!isUrl(url)) url = 'https://www.google.com/search?q=' + url;
+        else if (!(url.startsWith('https://') || url.startsWith('http://'))) url = 'http://' + url;
+
+
+        window.location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
+    });
+});
+
+function isUrl(val = ''){
+    return /^http(s?):\/\//.test(val) || val.includes('.') && val.substr(0, 1) !== ' ';
+}
