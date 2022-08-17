551cf06d893e6c113f65c9f4dc0a39c41ad94075
reoccurcat <reoccurcat@gmail.com>
2022-08-16 20:46:17 -0500

    oops this might help

--- public/assets/uv/uv.sw.js
+++ public/assets/uv/uv.sw.js
@@ -1,5 +1,5 @@
-importScripts('/uv/uv.bundle.js');
-importScripts('/uv/uv.config.js');
+importScripts('assets/uv/uv.bundle.js');
+importScripts('assets/uv/uv.config.js');
 
 class UVServiceWorker extends EventEmitter {     
     constructor(config = __uv$config) {
