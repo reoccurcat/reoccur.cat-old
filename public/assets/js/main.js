e4d6cfa02212de4f587b7a2da9e1dd2d2e500453
reoccurcat <reoccurcat@gmail.com>
2022-08-17 21:40:07 -0500

    test

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -145,26 +145,3 @@ function smth() {
         document.cookie = `ZmFpbGVkX2F1dGhlbnRpY2F0aW9u=dHJ1ZQ==`
     }
 }
-
-function immediate() {
-    let button = document.getElementById('buttonThing')
-    let copy = document.getElementById('othersad')
-
-    function readCookie(name) {
-        let nameEQ = name + "=";
-        let ca = document.cookie.split(';');
-        for (let i = 0; i < ca.length; i++) {
-            let c = ca[i];
-            while (c.charAt(0) === ' ') c = c.substring(1, c.length);
-            if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
-        }
-        return null;
-    }
-
-    let cookieread = readCookie('ZmFpbGVkX2F1dGhlbnRpY2F0aW9u')
-    if (!cookieread === null) {
-        copy.removeAttribute('hidden')
-    } else {
-        button.removeAttribute('hidden')
-    }
-}
