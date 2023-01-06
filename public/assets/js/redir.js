727a97d5dca1b95a8e41ff355d9b7927460cf0e8
reoccurcat <meow@reoccur.cat>
2023-01-06 10:40:50 -0600

    fix discord link

--- public/assets/js/redir.js
+++ public/assets/js/redir.js
@@ -8,5 +8,5 @@ function countDown() {
 }
 
 function changeCount() {
-    setInterval(function () { if (num === 0) {window.location.replace("https://discord.gg/rTC5N8XCQR")} else {countDown(num)} }, 1000)
+    setInterval(function () { if (num === 0) {window.location.replace("https://discord.gg/BfpRSNYxAB")} else {countDown(num)} }, 1000)
 }
